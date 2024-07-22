3eca0c587d1efde27aadbad7490b32d3534c12c8
ERNEST CHRIS <ernestchris6@gmail.com>
2024-07-22 22:44:54 +0100

    dispatching prev actions

--- src/App.js
+++ src/App.js
@@ -3,21 +3,37 @@ import { useReducer, useState } from "react";
 function reducer(state, action){
     console.log(state, action);
 
+    // return {count: 0, step: 1}
+
     // return state + action
-    if(action.type === "inc") return state + 1;
-    if(action.type === "dec") return state - 1;
-    if(action.type === "setCount") return action.payload;
+    // if(action.type === "inc") return state + 1;
+    // if(action.type === "dec") return state - 1;
+    // if(action.type === "setCount") return action.payload;
+
+    switch(action.type){
+        case "dec": 
+        return {...state, count: state.count -1}
+        case "inc": 
+        return{...state, count: state.count +1}
+        case "setCount": 
+        return {...state, count: action.payload}
+        default:
+          throw new Error("Unknown action")
+    }
 }
 
 function DateCounter() {
   //dispatch is like a setState here 0 is the state 
   //while 1 and -1 are the actions.payload
-  const [count, dispatch] = useReducer(reducer, 0)
+  // const [count, dispatch] = useReducer(reducer, 0)
   
   //const [count, setCount] = useState(0);
   // const [step, setStep] = useState(1);
 
-  
+  const initialState = {count: 0, step: 1};
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const {count, step} = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -48,7 +64,7 @@ function DateCounter() {
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
