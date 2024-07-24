5e060df6a03004c8b62dd771456ac95b9ca7016a
ERNEST CHRIS <ernestchris6@gmail.com>
2024-07-24 23:55:59 +0100

    other changes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ const initialState = {count: 0, step: 1};
 function reducer(state, action){
     console.log(state, action);
 
-    return {count: 0, step: 1}
+    // return {count: 0, step: 1}
 
-    return state + action
-    if(action.type === "inc") return state + 1;
-    if(action.type === "dec") return state - 1;
-    if(action.type === "setCount") return action.payload;
+    // return state + action
+    // if(action.type === "inc") return state + 1;
+    // if(action.type === "dec") return state - 1;
+    // if(action.type === "setCount") return action.payload;
 
     switch(action.type){
         case "dec": 
