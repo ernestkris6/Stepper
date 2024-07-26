c0c91e1a7365765905964565118d6c8aedc7f146
ERNEST CHRIS <ernestchris6@gmail.com>
2024-07-26 23:00:35 +0100

    comments

--- src/App.js
+++ src/App.js
@@ -49,8 +49,8 @@ function DateCounter() {
   const dec = function () {
     dispatch({type: "dec"})
     dispatch(-1)
-    setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - 1);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
