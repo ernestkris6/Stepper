21022c877d8908b3720cdea3d3a10aa08d7a6d9d
ERNEST CHRIS <ernestchris6@gmail.com>
2024-08-18 23:26:27 +0100

    btn

--- src/Components/Option.js
+++ src/Components/Option.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Option({question, dispatch}) {
   return (
     <div>
-        {/* {question.options.map((index, option)=> {
+        {question.options.map((index, option)=> {
             <button>
                 {index.option}
             </button>
-        })} */}
+        })}
     </div>
   )
 }
